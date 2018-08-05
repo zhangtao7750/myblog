c32de1cf6693fe3e9846fd2f93bea17e8a814f37
zhangtao7750 <38513722+zhangtao7750@users.noreply.github.com>
2018-08-05 15:39:48 +0800

    first commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mybox',
+        'USER':'root',
+        'PASSWORD':'Zt@6521033',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
